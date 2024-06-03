2a47020e6c9eb540fdc6de95815539cd278866d9
memac <maciasplayers@gmail.com>
2024-06-03 17:29:34 -0500

    Initial commit

---
+++ src/asgi.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
+
+# App Details
+digital_staff = FastAPI(
+    title="Digital Staff Backend",
+    summary="Delegate tasks to Agents",
+    version="1",
+    swagger_ui_parameters={
+        "syntaxHighlight.theme": "obsidian",
+        "docExpansion": "none"
+    }
+)
+
+digital_staff.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"]
+)
+
+
+@digital_staff.get("/", include_in_schema=False)
+async def docs_redirect():
+    return RedirectResponse(url="/docs")
