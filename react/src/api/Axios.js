12a5c78fafd52f3bdb825855f5e5dfc61b3fc2b7
Dejan Savic <savic.dejan.business@gmail.com>
2024-08-19 15:36:11 -0500

    axios update

--- react/src/api/Axios.js
+++ react/src/api/Axios.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 const axiosFormData = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "http://127.0.0.1:8080",
     headers: {
         "Content-Type": "multipart/form-data"
     }
 })
 
 const axiosJSON = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "http://127.0.0.1:8080",
     headers: {
         "Content-Type": "application/json"
     }
